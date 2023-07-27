a349430d22adba4b095d023ed39c688a26e91473
Wasiq-Usman <140292519+Wasiq-Usman@users.noreply.github.com>
2023-07-27 19:34:14 +0500

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,5 +1,9 @@
 import random
-num = int(input("Enter num to specify range"))
-num = random.randint(0,num)
-print(num)
+n = int(input("Enter no of elements you want in your list: "))
+l=[]
+num = int(input("Enter num to specify range: "))
+for i in range (1,n+1,1):
+    num = random.randint(0,num)
+    l.append(num)
+print("Random numbers list:", l)
 input ("Enter any key to exit")
