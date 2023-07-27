515277e2d37a0883c88539cd82f6a36f0c9eeb32
Wasiq-Usman <140292519+Wasiq-Usman@users.noreply.github.com>
2023-07-27 18:36:01 +0500

    Add files via upload

---
+++ Mississippi.py
@@ -0,0 +1,13 @@
+def count_s(word):
+    count = 0
+    for char in word:
+        if char == 's' or char == 'S':
+            count += 1
+    return count
+
+word = "mississippi"
+result = count_s(word)
+print(f"The number of 's' or 'S' in the word '{word}' is: {result}")
+
+
+
