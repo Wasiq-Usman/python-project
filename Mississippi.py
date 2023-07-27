edf7be78e1a72f8e18becece37d06bac2d3c1e48
Wasiq-Usman <140292519+Wasiq-Usman@users.noreply.github.com>
2023-07-27 19:38:38 +0500

    Update Mississippi.py

--- Mississippi.py
+++ Mississippi.py
@@ -4,7 +4,6 @@ def count_s(word):
         if char == 's' or char == 'S':
             count += 1
     return count
-
 word = "mississippi"
 result = count_s(word)
 print(f"The number of 's' or 'S' in the word '{word}' is: {result}")
